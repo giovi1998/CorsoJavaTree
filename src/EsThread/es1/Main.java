4520f8c1cfca4149a9e91e9ef7fefdcdf88d92e5
giovi1998 <giovicidu@gmail.com>
2021-06-29 14:54:19 +0200

    Es con i thread e dove utilizzo start,join,run

--- src/EsThread/es1/Main.java
+++ src/EsThread/es1/Main.java
@@ -9,9 +9,22 @@ public class Main {
         Sommatore s1 = new Sommatore(array,0,500000);
         Sommatore s2 = new Sommatore(array,500000,1000000);
         s1.start();
+        int n=500;
+        for (int i = 0; i <n ; i++) {
+
+            System.out.println("ciao");
+        }
+
         s2.start();
-        System.out.println(s1.somma);
-        System.out.println(s2.somma);
+        
+
+        try {
+            s1.join();
+            s2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } ;
+
         int sommaFinale = s1.somma+ s2.somma;
         System.out.println(sommaFinale);
     }
