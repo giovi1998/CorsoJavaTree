d5792b7c64c5386922171d6e4d2f5063912842d1
giovi1998 <giovicidu@gmail.com>
2021-07-02 11:52:02 +0200

    Stream

---
+++ src/stream/es1/es1.java
@@ -0,0 +1,18 @@
+package stream.es1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+
+Scrivere un programma Java che restituisca, a partire da una lista di nomi, la somma totale
+delle lettere contenute in ogni nome che inizia con la lettera "f"
+
+Suggerimento: realizzatelo prima senza Stream, poi con Stream
+
+ */
+public class es1 {
+    List<String> nomi= new  ArrayList<String>();
+    nomi.add("ciao");
+
+}
